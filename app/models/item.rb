d68e6fa463fcfa40a7cb3a7ef5c7542861bf89d0
tomekichi49 <taka.nagae49@gmail.com>
2024-06-12 12:33:48 +0900

    ActiveHashの導入とバリデーション、商品出品ページを表示できるようにする

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,4 +3,21 @@ class Item < ApplicationRecord
   has_one :purchase
 
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+  belongs_to_active_hash :condition
+  belongs_to_active_hash :shipping_fee
+  belongs_to_active_hash :prefecture
+  belongs_to_active_hash :shipping_day
+  
+  validates :image, presence: true
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :condition_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :shipping_fee_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :shipping_day_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, only_integer: true }
 end
