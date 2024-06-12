099f0ef8878a61f21280f6499851061d9b83f76b
tomekichi49 <taka.nagae49@gmail.com>
2024-06-12 18:36:51 +0900

    金額の必要な要素を取得する

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,3 +1,8 @@
-window.addEventListener('turbo:load', () => {
-  console.log("OK");
-});
+const priceInput = document.getElementById("item-price");
+console.log(priceInput);
+
+const commissionFee = document.getElementById("add-tax-price");
+console.log(commissionFee);
+
+const totalProfit = document.getElementById("profit");
+console.log(totalProfit);
