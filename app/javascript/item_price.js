93813f8d949e1727dfe809114afacb063a1c0807
tomekichi49 <taka.nagae49@gmail.com>
2024-06-13 15:17:19 +0900

    商品出品機能修正3

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,21 +1,25 @@
-document.addEventListener("DOMContentLoaded", () => {
+function post() {
   const priceInput = document.getElementById("item-price");
   const addTaxPrice = document.getElementById("add-tax-price");
   const profitElement = document.getElementById("profit");
 
-  priceInput.addEventListener("input", () => {
-    const inputValue = parseFloat(priceInput.value);
+  if (priceInput && addTaxPrice && profitElement) {
+    priceInput.addEventListener("input", () => {
+      const inputValue = parseFloat(priceInput.value);
 
-    if (!isNaN(inputValue) && inputValue > 0) {
-      const commissionRate = 0.1;
-      const commissionFee = Math.floor(inputValue * commissionRate);
-      const profit = Math.floor(inputValue - commissionFee);
+      if (!isNaN(inputValue) && inputValue > 0) {
+        const commissionRate = 0.1;
+        const commissionFee = Math.floor(inputValue * commissionRate);
+        const profit = Math.floor(inputValue - commissionFee);
 
-      addTaxPrice.innerHTML = commissionFee;
-      profitElement.innerHTML = profit;
-    } else {
-      addTaxPrice.innerHTML = 0;
-      profitElement.innerHTML = 0;
-    }
-  });
-});
+        addTaxPrice.innerHTML = commissionFee;
+        profitElement.innerHTML = profit;
+      } else {
+        addTaxPrice.innerHTML = 0;
+        profitElement.innerHTML = 0;
+      }
+    });
+  }
+}
+
+document.addEventListener('turbo:load', post);
--- app/models/item.rb
+++ app/models/item.rb
@@ -11,7 +11,6 @@ class Item < ApplicationRecord
   belongs_to_active_hash :prefecture
   belongs_to_active_hash :shipping_day
 
-  validates :user, presence: true
   validates :image, presence: true
   validates :name, presence: true
   validates :description, presence: true
