76c0433759c66d54a387beb407b75ee354f60ba8
tomekichi49 <taka.nagae49@gmail.com>
2024-06-16 19:04:58 +0900

    attr_accessorを用いて、保存したい複数のテーブルのカラム名すべてを扱えるようにする

--- app/models/purchase_form.rb
+++ app/models/purchase_form.rb
@@ -1,5 +1,10 @@
 class PurchaseForm
   include ActiveModel::Model
 
+  # 購入情報用の属性
+  attr_accessor :user_id, :item_id
+
+  # 発送先情報用の属性
+  attr_accessor :postal_code, :prefecture_id, :city, :addresses, :building, :phone_number
 
 end
