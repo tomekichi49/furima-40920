7a44cb299a143d7b79e661f7fa0d69e9f612fc9d
tomekichi49 <taka.nagae49@gmail.com>
2024-06-16 19:05:38 +0900

    バリデーションの処理を書く

--- app/models/purchase_form.rb
+++ app/models/purchase_form.rb
@@ -7,4 +7,15 @@ class PurchaseForm
   # 発送先情報用の属性
   attr_accessor :postal_code, :prefecture_id, :city, :addresses, :building, :phone_number
 
+  # バリデーションの定義
+  with_options presence: true do
+    validates :user_id
+    validates :item_id
+    validates :postal_code, format: { with: /\A\d{3}-\d{4}\z/, message: 'はハイフンを含めて入力してください' }
+    validates :prefecture_id, numericality: { other_than: 1, message: "を選択してください" }
+    validates :city
+    validates :addresses
+    validates :phone_number, format: { with: /\A\d{10,11}\z/, message: 'は10桁または11桁の半角数字で入力してください' }
+  end
+  
 end
