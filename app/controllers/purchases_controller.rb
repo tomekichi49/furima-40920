cd4b1998ab5aa810a6dcc25c2abb90911907cc01
tomekichi49 <taka.nagae49@gmail.com>
2024-06-17 15:30:56 +0900

    購入のためのフォームを実装する

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,13 +1,14 @@
 class PurchasesController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_item, only: [:index, :create]
 
   def index
     @purchase_form = PurchaseForm.new
   end
 
   def create
-    @purchase_form = PurchaseForm.new(purchase_params)
-    
+    @purchase_form = PurchaseForm.new(purchase_params.merge(user_id: current_user.id, item_id: @item.id))
+
     if @purchase_form.save
       redirect_to root_path
     else
@@ -17,7 +18,11 @@ class PurchasesController < ApplicationController
 
   private
 
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
   def purchase_params
-    params.require(:purchase_form).permit(:user_id, :item_id, :postal_code, :prefecture_id, :city, :addresses, :building, :phone_number)
+    params.require(:purchase_form).permit(:postal_code, :prefecture_id, :city, :addresses, :building, :phone_number)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
 
   root to: 'items#index'
-  
+
   resources :items do
-    resources :purchases, only: [:new, :create]
+    resources :purchases, only: [:index, :create]
   end
 end
