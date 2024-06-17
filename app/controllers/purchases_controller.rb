bc29adcd2c2991f153cd97bdacf5d75799e5e5a0
tomekichi49 <taka.nagae49@gmail.com>
2024-06-17 12:29:06 +0900

    コントローラーへ、Formオブジェクトのインスタンスを生成する記述をする

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,2 +1,24 @@
 class PurchasesController < ApplicationController
+
+  def index
+  end
+
+  def new
+    @purchase_form = PurchaseForm.new
+  end
+
+  def create
+    @purchase_form = PurchaseForm.new(purchase_params)
+    if @purchase_form.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def purchase_params
+    params.require(:purchase_form).permit(:user_id, :item_id, :postal_code, :prefecture_id, :city, :addresses, :building, :phone_number)
+  end
 end
