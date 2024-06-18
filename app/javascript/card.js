ee904124b8c0e084959faae042295708ee98541f
tomekichi49 <taka.nagae49@gmail.com>
2024-06-18 17:13:15 +0900

    ブラウザのカード情報を取得する、トークンが発行されたことを確認する公開鍵と共にカード情報をPAY.JP側へ送る、

---
+++ app/javascript/card.js
@@ -0,0 +1,25 @@
+const pay = () => {
+  const payjp = Payjp('pk_test_ef3884a0e1b6279f141409d9')// PAY.JPテスト公開鍵
+  const elements = payjp.elements();
+  const numberElement = elements.create('cardNumber');
+  const expiryElement = elements.create('cardExpiry');
+  const cvcElement = elements.create('cardCvc');
+
+  numberElement.mount('#number-form');
+  expiryElement.mount('#expiry-form');
+  cvcElement.mount('#cvc-form');
+
+  const form = document.getElementById('charge-form')
+  form.addEventListener("submit", (e) => {
+    payjp.createToken(numberElement).then(function (response) {
+      if (response.error) {
+      } else {
+        const token = response.id;
+        console.log(token)
+      }
+    });
+    e.preventDefault();
+  });
+};
+
+window.addEventListener("turbo:load", pay);
